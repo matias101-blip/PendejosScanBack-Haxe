83f62f393b6690ad2af42713fb69acfa4a8f3765
Thehunter101 <thehunter10121@gmail.com>
2024-12-20 02:45:38 -0500

    Update en la base de datos

--- src/Server.hx
+++ src/Server.hx
@@ -1,6 +1,16 @@
+import php.Global;
+import haxe.Json;
+import tjson.TJSON;
+import tink.http.Client;
 import tink.http.containers.*;
 import tink.http.Response;
 import tink.web.routing.*;
+
+typedef Proyectos = {
+    var Name:String;
+    var caps:Array<Int>;
+    var info:String;
+} 
 
 class Server{
     static function main() {
@@ -21,14 +31,28 @@ class Root {
     public function home() {
        return "Estas en el inicio";
     }
-    
-    @:get('/$name')
-    public function hello(name = 'World')
-        return 'Hello, $name! como andamos';
+  
 
-    @:get('/proyectos/Mangas')
-    public function manga(name="Elden ring") {
-        return 'Esto es el manga de $name!!! :3';
+    @:get('/proyectos')
+    @:get('/proyectos/$name')
+    public function manga(name:String = "") {
+        final url = 'https://my-json-server.typicode.com/matias101-blip/PendejosScanBack-Haxe/Proyectos';
+        return Client.fetch(url).all().map(function(o) switch o {
+            case Success(res):
+                var resBody:Array<Proyectos> = TJSON.parse(res.body.toBytes().toString());
+                if (name.length != 0){
+                    for (obj in resBody){
+                        if (obj.Name == name){
+                            return TJSON.encode(obj,'fancy');
+                        }
+                    }
+                    return Json.stringify({'nope':false});
+                }else{
+                    return TJSON.encode(resBody, 'fancy');
+                }
+            case Failure(error):
+                return 'Fail!!!: $error';
+        });
     }
 
     @:get('/api/user')
