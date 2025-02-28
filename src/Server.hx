a66494a78b7932be0f4e0c380da4be139f3126be
Thehunter101 <thehunter10121@gmail.com>
2025-02-27 22:55:09 -0500

    insert test

--- src/Server.hx
+++ src/Server.hx
@@ -130,7 +130,7 @@ class Root {
     @:post('api/InsertManga')
     @:bodyParam
     public function recibirData(body:MangaData){
-            return 'Datos recibidos ${body.name + body.resumen + body.generos + body.status} UwU 3';
+            return 'Datos recibidos ${body.name + body.resumen + body.generos + body.status} UwU 33';
     }
 
 }
