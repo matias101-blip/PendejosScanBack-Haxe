b854d101dca02fa19ffac96c19bb9d3259d3473e
Thehunter101 <thehunter10121@gmail.com>
2025-02-27 23:00:54 -0500

    insert test

--- src/Server.hx
+++ src/Server.hx
@@ -130,7 +130,7 @@ class Root {
     @:post('api/InsertManga')
     @:bodyParam
     public function recibirData(body:MangaData){
-            return 'Datos recibidos ${body.name + body.resumen + body.generos + body.status} UwU 3';
+            return 'Todo piola bb, nada fallo mas alla de tus nalgas UwU';
     }
 
 }
